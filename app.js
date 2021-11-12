a92918790128cc8e387773e0e16bbf021d079ff4
ValdezGonzalezGael <gael.valdez456@gmail.com>
2021-11-11 21:42:05 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import* as bodyParser from 'body-parser';
+import* as path from 'path';
+import multer from 'multer';
+import { Worker, isMainThread, workerData} from 'worker_threads';
+
+require('dotenv').config();
+
+const storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: function(req,file,cb){
+     cb(null, file.fieldname + '-'+Date.now() + path.extname(file.originalname));    
+    }
+});
+
+const upload = multer({dest:'uploads',storage: storage });
+
+const app= express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }));
+
+app.set("views", path.join(__dirname,"views"));
+app.set("view engine", "hbs");
+
+app.get("/",(req,res) => {
+   res.render("index");
+});
+
+app.post("/upload-video",upload.single("ssvideo"),(req,res)=>{
+    if(isMainThread){
+
+        let thread = new Worker("./threads/worker.js",{
+            workerData:{
+                file: req.file.path,
+                filename: req.file.filename
+            }
+        });
+
+        thread.on("message",(data)=>{
+            res.download(data.file, req.file.filename);
+        });
+
+        thread.on("error",(err)=> {
+            console.log("Error en el thread", err);
+        });
+
+        thread.on("exit",(code)=>{
+            if(code !=0){
+                console('El hilo se detuvo con el código de salida: ${code}');
+            }
+          })
+        
+        }
+        
+});
+
+const PORT = 8080;
+        app.listen(PORT,()=>{
+            console.log('El servidor inició por el puerto ${PORT}');
+        });
